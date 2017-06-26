bb787589427586fdea76127206c1aafc2745c7fe
DESKTOP-SAVNOE0\KJY <cheld414@daum.net>
2017-06-26 19:42:06 +0900

    make frame of "planMainPage"(css, html)

--- KoreanTypeAgile/startPage/models.py
+++ KoreanTypeAgile/startPage/models.py
@@ -6,3 +6,14 @@ class User(models.Model):
     userPassword = models.CharField(max_length=40)
     def __str__(self):
         return self.userId
+        
+class Todo(models.Model):
+    userId = models.CharField(max_length=20)
+
+    todoName = models.CharField(max_length=40)
+    todoContents = models.CharField(max_length=40)
+    startDate = models.DateTimeField()#날짜, 시간을 나타냄, python datetime.datetime 인스턴스로 표현됨
+    endDate = models.DateTimeField()
+    
+    def __str__(self):
+        return self.todoName
